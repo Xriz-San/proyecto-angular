1cb09941c783a87834878a9a9a6e3bb4387e3b3c
Xriz <xrizsanz@gmail.com>
2020-04-23 15:15:42 -0400

    Creacion de componentes y sus rutas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './component/about/about.component';
+import { ProjectsComponent } from './component/projects/projects.component';
+import { CreateComponent } from './component/create/create.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { ErrorComponent } from './component/error/error.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
